ee980aa771581c99e3efbf3e0c1c7188f79d50d8
AvniSoni19 <avnisoni21658@gmail.com>
2024-01-27 22:28:34 +0530

    The Report Problem

--- SQL/The_Report.sql
+++ SQL/The_Report.sql
@@ -1,3 +1,7 @@
 /*
 You are given two tables: Students and Grades. Students contains three columns ID, Name and Marks.
 */
+
+SELECT IF(Grades.Grade >= 8,Students.Name,"NULL"), Grades.Grade, Students.Marks 
+FROM Students, Grades
+WHERE Students.Marks >= Grades.Min_Mark AND Students.Marks <= Grades.Max_Mark
